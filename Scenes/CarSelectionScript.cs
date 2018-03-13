28664d3bddcb5b25e85eb1f5e6edc74847298f72
ramius84 <hiddenworks11@gmail.com>
2018-03-13 22:36:33 +0100

    Fixed city selection script, cleanup
    Removed unused lines and functions in scripts

---
+++ Scenes/CarSelectionScript.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class CarSelectionScript : MonoBehaviour {
+
+    //private List<GameObject> cars;
+    private int selectionIndex;
+
+    private GameObject[] carsList;
+
+	// Use this for initialization
+	void Start () {
+
+        selectionIndex = PlayerPrefs.GetInt("Chosencar");
+
+        carsList = new GameObject[transform.childCount];
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            carsList[i] = transform.GetChild(i).gameObject;
+        }
+
+        foreach (GameObject go in carsList)
+        {
+            go.SetActive(false);
+        }
+
+        if (carsList[selectionIndex])
+        {
+            carsList[selectionIndex].SetActive(true);
+        }
+    }
+
+    public void ToggleLeft()
+    {
+        carsList[selectionIndex].SetActive(false);
+        selectionIndex--;
+
+        if (selectionIndex < 0)
+        {
+            selectionIndex = carsList.Length - 1;
+        }
+        carsList[selectionIndex].SetActive(true);
+        Debug.Log(selectionIndex);
+    }
+
+    public void ToggleRight()
+    {
+        carsList[selectionIndex].SetActive(false);
+        selectionIndex++;
+
+        if (selectionIndex >= carsList.Length)
+        {
+            selectionIndex = 0;
+        }
+        carsList[selectionIndex].SetActive(true);
+        Debug.Log(selectionIndex);
+    }
+
+    public void ConfirmButton()
+    {
+        PlayerPrefs.SetInt("ChosenCar", selectionIndex);
+        SceneManager.LoadScene(1);
+    }
+
+    //private void Update()
+    //{
+    //    if (Input.GetButtonDown("Right"))
+    //    {
+    //        carsList[selectionIndex].SetActive(false);
+    //        selectionIndex++;
+
+    //        if (selectionIndex >= carsList.Length)
+    //        {
+    //            selectionIndex = 0;
+    //        }
+    //        carsList[selectionIndex].SetActive(true);
+    //    }
+    //    if (Input.GetButtonDown("Left"))
+    //    {
+    //        carsList[selectionIndex].SetActive(false);
+    //        selectionIndex--;
+
+    //        if (selectionIndex < 0)
+    //        {
+    //            selectionIndex = carsList.Length - 1;
+    //        }
+    //        carsList[selectionIndex].SetActive(true);
+    //    }
+    //    if (Input.GetButtonDown("Submit"))
+    //    {
+    //        PlayerPrefs.SetInt("ChosenCar", selectionIndex);
+    //        SceneManager.LoadScene(1);
+    //    }
+    //}
+}
---
+++ Scenes/MenuAppearScript.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuAppearScript : MonoBehaviour
+{
+    public bool czewaActive = false;
+    public bool katoActive = false;
+    public bool krakActive = false;
+    public GameObject czewaMenuPrefab;
+    public GameObject katoMenuPrefab;
+    public GameObject krakMenuPrefab;
+    public GameObject[] cityMenus;
+    public enum Cities { empty, Czewa, Kato, Krak };
+    public Cities chosenCity;
+
+    private void Update()
+    {
+        switch (chosenCity)
+        {
+            case Cities.Czewa:
+                czewaMenuPrefab.SetActive(czewaActive);
+                break;
+
+            case Cities.Kato:
+                katoMenuPrefab.SetActive(katoActive);
+                break;
+
+            case Cities.Krak:
+                krakMenuPrefab.SetActive(krakActive);
+                break;
+
+            case Cities.empty:
+            default:
+                czewaMenuPrefab.SetActive(false);
+                katoMenuPrefab.SetActive(false);
+                krakMenuPrefab.SetActive(false);
+                break;
+        }
+    }
+
+    public void OnEmptyClick()
+    {
+        czewaActive = false;
+        katoActive = false;
+        krakActive = false;
+        czewaMenuPrefab.SetActive(czewaActive);
+        katoMenuPrefab.SetActive(katoActive);
+        krakMenuPrefab.SetActive(krakActive);
+        chosenCity = Cities.Czewa;
+        Debug.Log("CZEWA: " + czewaActive);
+        Debug.Log("KATO :" + katoActive);
+        Debug.Log("KRAK :" + krakActive);
+        Debug.Log("");
+    }
+
+    public void OnCzewaCityClick()
+    {
+        czewaActive = false;
+        katoActive = false;
+        krakActive = false;
+        czewaActive = !czewaActive;
+        czewaMenuPrefab.SetActive(czewaActive);
+        katoMenuPrefab.SetActive(katoActive);
+        krakMenuPrefab.SetActive(krakActive);
+        chosenCity = Cities.Czewa;
+        Debug.Log("CZEWA: " + czewaActive);
+        Debug.Log("KATO :" + katoActive);
+        Debug.Log("KRAK :" + krakActive);
+        Debug.Log("");
+    }
+
+    public void OnKatoCityClick()
+    {
+        czewaActive = false;
+        katoActive = false;
+        krakActive = false;
+        katoActive = !katoActive;
+        katoMenuPrefab.SetActive(katoActive);
+        czewaMenuPrefab.SetActive(czewaActive);
+        krakMenuPrefab.SetActive(krakActive);
+        chosenCity = Cities.Kato;
+        Debug.Log("CZEWA: " + czewaActive);
+        Debug.Log("KATO :" + katoActive);
+        Debug.Log("KRAK :" + krakActive);
+        Debug.Log("");
+    }
+
+    public void OnKrakCityClick()
+    {
+        czewaActive = false;
+        katoActive = false;
+        krakActive = false;
+        krakActive = !krakActive;
+        czewaMenuPrefab.SetActive(czewaActive);
+        katoMenuPrefab.SetActive(katoActive);
+        krakMenuPrefab.SetActive(krakActive);
+        chosenCity = Cities.Krak;
+        Debug.Log("CZEWA: " + czewaActive);
+        Debug.Log("KATO :" + katoActive);
+        Debug.Log("KRAK :" + krakActive);
+        Debug.Log("");
+    }
+}
